2071d531125a438c407cd1bcb207e2e7135f7069
Diego Montoya <dmontoya@econtainers.co>
2020-11-24 21:57:14 -0500

    quicklink implementation for Angular. It provides router preloading strategy which automatically downloads the lazy-loaded modules associated with all the visible links on the screen.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuicklinkModule, QuicklinkStrategy } from 'ngx-quicklink';
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
-})],
-  exports: [RouterModule]
+  imports: [
+    QuicklinkModule,
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: QuicklinkStrategy,
+      initialNavigation: 'enabled'
+    })
+  ],
+  exports: [
+    RouterModule,
+    QuicklinkModule
+  ]
 })
 export class AppRoutingModule { }
